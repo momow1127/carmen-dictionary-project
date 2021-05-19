eefe725476dd7c08258593ceefb476bf911550e4
momow1127 <wong.mo1127@gmail.com>
2021-05-18 20:47:59 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.jpg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" className="img-fluid" />
+        Hello
+
+      </header>
+    </div>
+  );
+}
