589b2b5029d5c2c4ae8e399ce05bdbd1642ff4bb
momow1127 <wong.mo1127@gmail.com>
2021-05-23 12:11:46 -0400

    update css and js

---
+++ src/Meaning.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Meaning.css";
+
+
+export default function Meaning(props) {
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <div className="definition">{definition.definition}</div>
+
+           
+          </div>
+        );
+      })}
+    </div>
+  );
+}
