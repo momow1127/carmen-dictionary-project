5a5f7ff2330236a25e151b2e436b104ba7ec73df
momow1127 <wong.mo1127@gmail.com>
2021-05-23 11:40:26 -0400

    update js and css

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ export default function App() {
     <div className="App">
         <div className="container">
           <header className="App-header">
-           <img src={logo} className="App-logo" alt="logo" className="img-fluid" />
-        Hello
+           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
         <main>
           <Dictionary />
         </main>
-        <footer classNAme="text-center"> Coded by Carmen Wong</footer>
+        <footer classNAme="text-center"> 
+        <small>Coded by Carmen Wong</small>
+        </footer>
     </div>
     </div>
   );
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from "axios";
+import "./Dictionary.css";
 
 export default function Dictionary (){
-    return "Hello from Dictionary";
+   let[keyword, setKeyword] = useState("");
+
+   function handleResponse(repsonse) {
+       console.log(response.data[0]);
+   }
+
+    function search(event){
+    event.preventDefault();
+    
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(handleResponse);
+
+    }
+
+    function handleKeywordChange(event) {
+        setKeyword(event.target.value);
+}
+
+    return(
+    <div className="Dictionary">
+        <form onSubmit={search}>
+            <input type="search" onChange={handleKeywordChange} />
+        </form>
+       </div>
+    );
 }
