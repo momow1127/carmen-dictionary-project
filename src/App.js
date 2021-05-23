0ff2b4f7da7d8cd942551edafd6ae46391388e8e
momow1127 <wong.mo1127@gmail.com>
2021-05-23 10:40:40 -0400

    update app js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.jpg';
 import './App.css';
+import Dictionary from'./Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" className="img-fluid" />
+        <div className="container">
+          <header className="App-header">
+           <img src={logo} className="App-logo" alt="logo" className="img-fluid" />
         Hello
-
-      </header>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer classNAme="text-center"> Coded by Carmen Wong</footer>
+    </div>
     </div>
   );
 }
